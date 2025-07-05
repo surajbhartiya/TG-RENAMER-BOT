73fa7b1c2cbaeae7fa90a0a1c68f31bd31b97fb1
surajbhartiya <surjibhartiya3232@gmail.com>
2025-07-05 13:00:39 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -26,7 +26,7 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "mongodb+srv://surajbhartiya3242:Rag6SHvR5CKD0ubo@cluster0.ggs0jdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "Rag6SHvR5CKD0ubo@cluster0.ggs0jdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     # https://t.me/hevcbay/951
     OUO_IO_API_KEY = ""
     # maximum message length in Telegram
